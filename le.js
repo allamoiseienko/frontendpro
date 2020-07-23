a091ef14c7b4fed685e739a686278699b18f41e5
Alla Moiseienko <al.moiseienko@gmail.com>
2020-07-23 22:30:06 +0300

    set&foo

--- le.js
+++ le.js
@@ -1,34 +1,57 @@
 function initSlider(size) {
     var count = 0;
-
     function up () {
-        console.log(++count);   
+        ++count;
+        if (size == count) {
+            count = 0    
+        } 
+        else if (count == 0) {
+            count = size
+        }
+        console.log(count); 
     }
     function down () {
-        console.log(--count);
-    }
-    function set () {
+        --count;
         if (size == count) {
-            count = 0;
+            count = 0   
+        }  
+        else if (count == 0) {
+            count = size
+        }
+        console.log(count);
+    }
+    function foo () {
+        console.log(count = 0);
+    }
+    function set (n) {
+        if (n < 0 && n > size) {
+            console.log('Error')
+        }
+        else {
+            count = n;
             console.log(count);
         }
     }
     return {
         up: up,
-        down: down,       
+        down: down, 
+        foo: foo,
+        set: set,      
     }
 }
-var slider = initSlider(2);
-
+var slider = initSlider(3);
 slider.up();
 slider.up();
 slider.down();
+slider.down();
+slider.down();
+slider.foo();
+slider.set(7);
+
   
 /*
 Уточнение по Дз - 7:
 InitSlider - возвращает 3 ф-ии (up, down, set) которые будут делать работу по изменению счетчика
-
-
 Пример : 
 
 var slider = InitSlider(10)
